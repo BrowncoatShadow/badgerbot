abce223a5c058d8ae229768d33c72f07afc79229
BrowncoatShadow <BrowncoatShadow@users.noreply.github.com>
2017-11-08 12:54:40 -0600

    Basic permissions and command documentation

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const bot = new Eris.CommandClient(process.env.DISCORD_TOKEN, {}, {
   prefix: (process.env.NODE_ENV === 'production') ? '.' : ','
 })
 
-const twitch = new tmi.client({
+const client = new tmi.client({
   options: {
     debug: (process.env.NODE_ENV === 'production') ? false : true
   },
@@ -42,46 +42,38 @@ const twitch = new tmi.client({
   channels: db.get('channels').value()
 })
 
-twitch.on('join', (channel, user, self) => {
+client.on('join', (channel, user, self) => {
   if (self) bot.createMessage(process.env.DISCORD_CHANNEL, `Joined ${channel}`)
 })
 
-twitch.on('part', (channel, user, self) => {
+client.on('part', (channel, user, self) => {
   if (self) bot.createMessage(process.env.DISCORD_CHANNEL, `Left ${channel}`)
 })
 
-twitch.on('chat', (channel, user, message, self) => {
+client.on('chat', (channel, user, message, self) => {
   bot.createMessage(process.env.DISCORD_CHANNEL,
     `[${channel}] <${user['display-name']}> ${message}`)
 })
 
-twitch.on('action', (channel, user, message, self) => {
+client.on('action', (channel, user, message, self) => {
   bot.createMessage(process.env.DISCORD_CHANNEL,
     `[${channel}] **${user['display-name']}** ${message}`)
 })
 
-bot.registerCommand('ping', 'Pong!', {
+bot.registerCommand('ping', (msg) => {
+  bot.addMessageReaction(msg.channel.id, msg.id, 'tentacan:355091430093488128')
+}, {
   description: 'Pong!'
 })
 
-bot.registerCommand('list', () => {
-  var list = twitch.getChannels()
-  if (list.length === 0) return 'Not connected to any channels.'
-  return 'Connected to: ' + list.join(', ')
-}, {
-  description: "List connected twitch channels",
-})
-
 bot.registerCommand('join', (msg, args) => {
-  if (args.length === 0) return 'Please specify at least one channel.'
-
   sanitizeChannelList(args).forEach(chan => {
-    if (twitch.getChannels().includes(chan)) {
+    if (client.getChannels().includes(chan)) {
       bot.createMessage(process.env.DISCORD_CHANNEL, `I am already in ${chan}`)
       return
     }
 
-    twitch.join(chan).then(data => {
+    client.join(chan).then(data => {
       db.get('channels').push(chan).write()
     }).catch(err => {
       bot.createMessage(process.env.DISCORD_CHANNEL, `${err}`)
@@ -89,18 +81,24 @@ bot.registerCommand('join', (msg, args) => {
   })
 }, {
   description: "Join a twitch channel's chat",
+  fullDescription: "Join a twitch channel's chat and relay it to discord",
+  usage: '<channel>...',
+  argsRequired: true,
+  requirements: {
+    permissions: {
+      administrator: true
+    }
+  }
 })
 
 bot.registerCommand('part', (msg, args) => {
-  if (args.length === 0) return 'Please specify at least one channel.'
-
   sanitizeChannelList(args).forEach(chan => {
-    if (!twitch.getChannels().includes(chan)) {
+    if (!client.getChannels().includes(chan)) {
       bot.createMessage(process.env.DISCORD_CHANNEL, `I am not in ${chan}`)
       return
     }
 
-    twitch.part(chan).then(data => {
+    client.part(chan).then(data => {
       db.get('channels').remove(chan).write()
     }).catch(err => {
       bot.createMessage(process.env.DISCORD_CHANNEL, `${err}`)
@@ -108,11 +106,28 @@ bot.registerCommand('part', (msg, args) => {
   })
 }, {
   description: "Leave a twitch channel's chat",
+  fullDescription: "Leave a twitch channel's chat and stop relaying it to discord",
+  usage: '<channel>...',
+  argsRequired: true,
+  requirements: {
+    permissions: {
+      administrator: true
+    }
+  }
+})
+
+bot.registerCommand('list', () => {
+  var list = client.getChannels()
+  if (list.length === 0) return 'I am not watching any twitch chats.'
+  return 'Watching: ' + list.join(', ')
+}, {
+  description: "List twitch channels being watched",
+  fullDescription: "List all the twitch channel chats being watched and relayed to discord"
 })
 
 bot.on('ready', () => {
   console.log('Ready!')
-  twitch.connect()
+  client.connect()
 })
 
 bot.connect()
